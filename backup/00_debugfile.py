887b034913bcb9685164bf9ea906b216de974aee
StanleyYuenTK <89916631+StanleyYuenTK@users.noreply.github.com>
2026-05-29 15:10:04 +0800

    Delete backup directory

--- backup/00_debugfile.py
+++
@@ -1,8 +0,0 @@
-import inspect
-import sys
-sys.path.append("..")
-import gnn_zoo  # 假設你的檔名是 final_ki.py
-
-functions_list = inspect.getmembers(gnn_zoo, inspect.isfunction)
-for name, _ in functions_list:
-    print(name)
